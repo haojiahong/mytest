8099c46985bb46bd52ba9bb59ce54f74be0939a4
haojiahong <jiahong.hao@tqmall.com>
2016-04-29 17:59:03 +0800

    继承 复合的区别

--- src/test/java/com/hao/mytest/test/HelloWorldTest.java
+++ src/test/java/com/hao/mytest/test/HelloWorldTest.java
@@ -1,6 +1,11 @@
 package com.hao.mytest.test;
 
+import com.hao.mytest.effectivejava.sixteen.InstrumentedSet;
 import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,10 +15,17 @@ import static org.junit.Assert.assertEquals;
 public class HelloWorldTest {
 
     @Test
-    public void testSayHello(){
+    public void testSayHello() {
         Main main = new Main();
         String result = main.sayHello();
 
-        assertEquals("hello maven",result);
+        assertEquals("hello maven", result);
+    }
+
+    @Test
+    public void testInstrumentSet() {
+        InstrumentedSet<String> s = new InstrumentedSet<String>(new HashSet<String>());
+        s.addAll(Arrays.asList("Snap", "Pop", "Crackle"));
+        System.out.printf("s的大小：" + s.getAddCount());
     }
 }
